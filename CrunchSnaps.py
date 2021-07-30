51ac18d7064b4f004fbc9fa5d7e798fc976c7be8
Mike Grudic <mike.grudich@gmail.com>
2021-07-29 21:18:40 -0700

    stuff

--- CrunchSnaps.py
+++ CrunchSnaps.py
@@ -1,4 +1,4 @@
-from multiprocessing import Pool
+from multiprocessing import Pool, cpu_count
 from tasks import *
 from natsort import natsorted
 import h5py
@@ -25,7 +25,7 @@ def DoTasksForSimulation(snaps=[], tasks=[], task_params=[]):
     if not task_params:
         N_params = len(snaps)
     else:
-        N_params = len(task_params)    
+        N_params = len(task_params[0])    
     
     # don't yet know what the snapshot times are - get the snapshot times in a prepass
     snaptimes = []
@@ -33,7 +33,7 @@ def DoTasksForSimulation(snaps=[], tasks=[], task_params=[]):
         with h5py.File(s, 'r') as F:
             snaptimes.append(F["Header"].attrs["Time"])
     snaptimes = np.array(snaptimes)
-    print(snaptimes)
+#    print(snaptimes)
     snapdict = dict(zip(snaptimes, snaps))
     
     # note that params must be sorted by time!
@@ -42,7 +42,7 @@ def DoTasksForSimulation(snaps=[], tasks=[], task_params=[]):
 
     snapdata_buffer = {} # should store data from at most 2 snapshots
     
-    for i in range(N_params):
+    for i in range(N_params): # this loop cannot be parallelized without sacrificing IO performance because it has a persistent IO buffer modified on the fly
         ######################### initialization  ############################################
         # initialize the task objects at figure out what data the tasks need
         
@@ -58,20 +58,22 @@ def DoTasksForSimulation(snaps=[], tasks=[], task_params=[]):
         # OK now we do file I/O if we don't find what we need in the buffer
         t1, t2 = snaptimes[snaptimes<=time][-1], snaptimes[snaptimes>=time][0]
         # do a pass to delete anything that will no longer be needed
-        print(list(snapdata_buffer.keys()))
+#        print(list(snapdata_buffer.keys()))
         for k in list(snapdata_buffer.keys()):
-            if k < t1 or k not in snaptimes: del snapdata_buffer[k] # delete if not needed for interpolation (including old interpolants)
+            if k < t1: del snapdata_buffer[k] # delete if not needed for interpolation (including old interpolants)
         for t in t1, t2:
             if not t in snapdata_buffer.keys(): snapdata_buffer[t] = GetSnapData(snapdict[t], required_snapdata)
 
         ##########################  interpolation #############################################
         # now get the particle data we need for this time, interpolating if needed
-        
+
+#        print(time, [k for k in snapdata_buffer.keys()])
         if time in snapdata_buffer.keys(): # if we have the snapshot for this exact time in the buffer, no interpolation needed
-            snapdata_for_thistime = snapdata_buffer[t]
+            snapdata_for_thistime = snapdata_buffer[time]
         else:
             print("interpolating to time ", time)
             snapdata_for_thistime = SnapInterpolate(time, t1, t2, snapdata_buffer)
+            snapdata_buffer[time] = snapdata_for_thistime
 
         ################# task execution  ####################################################
         # actually do the task - each method can optionally return data to be compiled in the pass through the snapshots
@@ -85,42 +87,43 @@ def SnapInterpolate(t,t1,t2,snapdata_buffer):
     stuff_to_interp_lin = "PartType0/Coordinates", "PartType0/Velocities", "PartType0/MagneticField", "PartType5/Coordinates", "PartType5/Velocities"
     stuff_to_interp_log = "PartType0/SmoothingLength", "PartType0/InternalEnergy", "PartType0/Pressure", "PartType0/SoundSpeed", "PartType0/Density", "PartType5/Masses"
     interpolated_data = snapdata_buffer[t1].copy()
-    idx1, idx2, id1_order, id2_order = {}, {}, {}, {}
-    for ptype in "PartType0", "PartType5":
-        id1 = snapdata_buffer[t1][ptype + "/ParticleIDs"]
-        id2 = snapdata_buffer[t2][ptype + "/ParticleIDs"]
+    print(interpolated_data["PartType0/Coordinates"][0])
+    # idx1, idx2, id1_order, id2_order = {}, {}, {}, {}
+    # for ptype in "PartType0", "PartType5":
+    #     id1 = snapdata_buffer[t1][ptype + "/ParticleIDs"]
+    #     id2 = snapdata_buffer[t2][ptype + "/ParticleIDs"]
 
-        if ptype == "PartType0": # if we have to worry about wind IDs
-            wind_idx1 = np.in1d(id1, wind_ids)
-            if np.any(wind_idx1):
-                progenitor_ids = np.int_(snapdata_buffer[t1]["PartType0/ParticleIDGenerationNumber"])[wind_idx1]
-                child_ids = np.int_(snapdata_buffer[t1]["PartType0/ParticleChildIDsNumber"])[wind_idx1]
-                wind_particle_ids = -((progenitor_ids << 16) + child_ids) # bit-shift the progenitor ID outside the plausible range for particle count, then add child ids to get a unique new id                                         
-                id1[wind_idx1] = wind_particle_ids
+    #     if ptype == "PartType0": # if we have to worry about wind IDs
+    #         wind_idx1 = np.in1d(id1, wind_ids)
+    #         if np.any(wind_idx1):
+    #             progenitor_ids = np.int_(snapdata_buffer[t1]["PartType0/ParticleIDGenerationNumber"])[wind_idx1]
+    #             child_ids = np.int_(snapdata_buffer[t1]["PartType0/ParticleChildIDsNumber"])[wind_idx1]
+    #             wind_particle_ids = -((progenitor_ids << 16) + child_ids) # bit-shift the progenitor ID outside the plausible range for particle count, then add child ids to get a unique new id                                         
+    #             id1[wind_idx1] = wind_particle_ids
 
-            wind_idx2 = np.in1d(id2, wind_ids)
-            if np.any(wind_idx2):
-                progenitor_ids = np.int_(snapdata_buffer[t2]["PartType0/ParticleIDGenerationNumber"])[wind_idx2]
-                child_ids = np.int_(snapdata_buffer[t2]["PartType0/ParticleChildIDsNumber"])[wind_idx2]
-                wind_particle_ids = -((progenitor_ids << 16) + child_ids) # bit-shift the progenitor ID outside the plausible range for particle count, then add child ids to get a unique new id                                         
-                id1[wind_idx2] = wind_particle_ids        
+    #         wind_idx2 = np.in1d(id2, wind_ids)
+    #         if np.any(wind_idx2):
+    #             progenitor_ids = np.int_(snapdata_buffer[t2]["PartType0/ParticleIDGenerationNumber"])[wind_idx2]
+    #             child_ids = np.int_(snapdata_buffer[t2]["PartType0/ParticleChildIDsNumber"])[wind_idx2]
+    #             wind_particle_ids = -((progenitor_ids << 16) + child_ids) # bit-shift the progenitor ID outside the plausible range for particle count, then add child ids to get a unique new id                                         
+    #             id2[wind_idx2] = wind_particle_ids        
         
-        common_ids = np.intersect1d(id1,id2)
-        id1_order[ptype] = id1.argsort()
-        id2_order[ptype] = id2.argsort()
-        idx1[ptype] = np.in1d(np.sort(id1),common_ids)
-        idx2[ptype] = np.in1d(np.sort(id2),common_ids)
+    #     common_ids = np.intersect1d(id1,id2)
+    #     id1_order[ptype] = id1.argsort()
+    #     id2_order[ptype] = id2.argsort()
+    #     idx1[ptype] = np.in1d(np.sort(id1),common_ids)
+    #     idx2[ptype] = np.in1d(np.sort(id2),common_ids)
         
-    for field in stuff_to_interp_lin:
-        if field in interpolated_data.keys():
-            if field == "Header": continue
-            ptype = field.split("/")[0]
-            interpolated_data[field] = snapdata_buffer[t1][field][id1_order[ptype]][idx1[ptype]] * (t2 - t)/(t2 - t1) + snapdata_buffer[t2][field][id2_order[ptype]][idx2[ptype]] * (t - t1)/(t2 - t1)
-    for field in stuff_to_interp_log:        
-        if field in interpolated_data.keys():
-            if field == "Header": continue
-            ptype = field.split("/")[0]
-            interpolated_data[field] = np.exp(np.log(snapdata_buffer[t1][field][id1_order[ptype]][idx1[ptype]]) * (t2 - t)/(t2 - t1) + np.log(snapdata_buffer[t2][field][id2_order[ptype]][idx2[ptype]]) * (t - t1)/(t2 - t1))
+    # for field in stuff_to_interp_lin:
+    #     if field in interpolated_data.keys():
+    #         if field == "Header": continue
+    #         ptype = field.split("/")[0]
+    #         interpolated_data[field] = snapdata_buffer[t1][field][id1_order[ptype]][idx1[ptype]] * (t2 - t)/(t2 - t1) + snapdata_buffer[t2][field][id2_order[ptype]][idx2[ptype]] * (t - t1)/(t2 - t1)
+    # for field in stuff_to_interp_log:        
+    #     if field in interpolated_data.keys():
+    #         if field == "Header": continue
+    #         ptype = field.split("/")[0]
+    #         interpolated_data[field] = np.exp(np.log(snapdata_buffer[t1][field][id1_order[ptype]][idx1[ptype]]) * (t2 - t)/(t2 - t1) + np.log(snapdata_buffer[t2][field][id2_order[ptype]][idx2[ptype]]) * (t - t1)/(t2 - t1))
     return interpolated_data
         
 
@@ -135,13 +138,22 @@ def GetSnapData(snappath, required_snapdata):
         
 from glob import glob
 
-tasks = [SurfaceDensityMap,]
+tasks = [SimVis,SimVis2]
 snaps = glob("M2e3*/output/snap*.hdf5")
 
 params = []
-for i in range(1080):
-    params.append({"Time": 0.001, "pan": float(i)/1080 * 360})
-params = [params,]
+Nangle = 360
+for i in range(Nangle):
+    params.append({"Time": 0.001, "pan": float(i)/Nangle * 360})
+#params = [params,]
+Nchunks = cpu_count()
+params_chunks = [params[i*len(params)//Nchunks:(i+1)*len(params)//Nchunks] for i in range(Nchunks)]
 
 
-DoTasksForSimulation(snaps, tasks, params)
+def DoStuff(params_chunk):
+    DoTasksForSimulation(snaps, tasks, [params_chunk,params_chunk])
+
+
+Pool(1).map(DoStuff, params_chunks)
+
+
--- tasks.py
+++ tasks.py
@@ -5,13 +5,12 @@ import matplotlib
 matplotlib.use('Agg')
 from matplotlib import pyplot as plt
 
-class SurfaceDensityMap:
+class SimVis:
     def __init__(self, params):
-        
-        
-        self.RequiredSnapdata = ["PartType0/Coordinates", "PartType0/Masses", "PartType0/SmoothingLength"]
+        self.RequiredSnapdata = ["PartType0/Coordinates", "PartType0/Masses", "PartType0/SmoothingLength","PartType0/ParticleIDGenerationNumber","PartType0/ParticleChildIDsNumber"]
         self.params = params
-        default_params = {"res": 128,
+
+        default_params = {"res": 1024,
                           "rmax": None,
                           "limits": None,
                           "center": None,
@@ -25,6 +24,18 @@ class SurfaceDensityMap:
     def GetRequiredSnapdata(self):
         return self.RequiredSnapdata            
 
+    def CoordinateTransform(self, x, m, h):
+        tilt, pan = self.params["tilt"], self.params["pan"]
+#        print(tilt, pan)
+        # first pan
+        cosphi, sinphi = np.cos(np.pi*pan/180), np.sin(np.pi*pan/180)
+        x[:] = np.c_[cosphi*x[:,0] + sinphi*x[:,2],x[:,1], -sinphi*x[:,0] + cosphi*x[:,2]]
+        # then tilt
+        costheta, sintheta = np.cos(np.pi*tilt/180), np.sin(np.pi*tilt/180)
+        x[:] = np.c_[x[:,0], costheta*x[:,1] + sintheta*x[:,2], -sintheta*x[:,1] + costheta*x[:,2]]
+        # then do projection
+            
+    
     def DoTask(self, snapdata):        
         if self.params["center"] is None:
             center = np.repeat(snapdata["Header"]["BoxSize"]*0.5,3)
@@ -33,31 +44,101 @@ class SurfaceDensityMap:
             rmax = snapdata["Header"]["BoxSize"]/10
         else: rmax = self.params["rmax"]
         if self.params["pan"] is None:
-            pan = 0
-        else: pan = self.params["pan"]
+            self.params["pan"] = 0
         if self.params["tilt"] is None:
-            tilt = 0
-        else:
-            tilt = self.params["tilt"]
+            self.params["tilt"] = 0
             
         res = self.params["res"]
         pos, mass, hsml = np.copy(snapdata["PartType0/Coordinates"]), np.copy(snapdata["PartType0/Masses"]), np.copy(snapdata["PartType0/SmoothingLength"]) # copy these because we don't want to modify them
-        hsml = np.clip(hsml,2*rmax/res, 1e100)
         pos -= center
-        sigma_gas = GridSurfaceDensity(mass, pos, hsml, np.zeros(3), 2*rmax, res=res).T        
-        fig, ax = plt.subplots(figsize=(6,6))
-        X = Y = np.linspace(-rmax, rmax, res)
-        X, Y = np.meshgrid(X, Y)
-        p = ax.pcolormesh(X, Y, sigma_gas, norm=matplotlib.colors.LogNorm(vmin=1,vmax=1e3))
-        ax.set_aspect('equal')
+        print(pos[0])
+        self.CoordinateTransform(pos,mass,hsml)
+        
+        hsml = np.clip(hsml,2*rmax/res, 1e100)
+        sigma_gas = GridSurfaceDensity(mass, pos, hsml, np.zeros(3), 2*rmax, res=res).T
+        f = (np.log10(sigma_gas)-np.log10(1))/(np.log10(1e3)-np.log10(1))
+        plt.imsave("sigma_gas_%s_%s.png"%(str(round(self.params["pan"])).zfill(4), str(round(self.params["tilt"])).zfill(4)), plt.get_cmap('viridis')(f))
+        # fig, ax = plt.subplots(figsize=(6,6))
+        # X = Y = np.linspace(-rmax, rmax, res)
+        # X, Y = np.meshgrid(X, Y)
+        # p = ax.pcolormesh(X, Y, sigma_gas, norm=matplotlib.colors.LogNorm(vmin=1,vmax=1e3))
+        # ax.set_aspect('equal')
 
-        divider = make_axes_locatable(ax)
-        cax = divider.append_axes("right", size="5%", pad=0.0)
-        fig.colorbar(p,label=r"$\Sigma_{\rm gas}$ $(\rm M_\odot\,pc^{-2})$",cax=cax)
-        ax.set_xlabel("X (pc)")
-        ax.set_ylabel("Y (pc)")
-        plt.savefig("sigma_gas_%g.png"%snapdata["Header"]["Time"],bbox_inches='tight',dpi=300)
-        plt.close()
-        print("done")
+        # divider = make_axes_locatable(ax)
+        # cax = divider.append_axes("right", size="5%", pad=0.0)
+        # fig.colorbar(p,label=r"$\Sigma_{\rm gas}$ $(\rm M_\odot\,pc^{-2})$",cax=cax)
+        # ax.set_xlabel("X (pc)")
+        # ax.set_ylabel("Y (pc)")
+        # plt.savefig("sigma_gas_%s_%s.png"%(str(round(self.params["pan"])).zfill(4), str(round(self.params["tilt"])).zfill(4)),bbox_inches='tight',dpi=300)
+        # plt.close()
+#        print("done")
         return sigma_gas    
 
+class SimVis2:
+    def __init__(self, params):
+        self.RequiredSnapdata = ["PartType0/Coordinates", "PartType0/Masses", "PartType0/SmoothingLength","PartType0/ParticleIDGenerationNumber","PartType0/ParticleChildIDsNumber"]
+        self.params = params
+
+        default_params = {"res": 256,
+                          "rmax": None,
+                          "limits": None,
+                          "center": None,
+                          "pan": None,
+                          "tilt": None
+                          }
+
+        for p in default_params.keys():
+            if not p in params.keys(): self.params[p] = default_params[p]
+
+    def GetRequiredSnapdata(self):
+        return self.RequiredSnapdata            
+
+    def CoordinateTransform(self, x, m, h):
+        tilt, pan = self.params["tilt"], self.params["pan"]
+#        print(tilt, pan)
+        # first pan
+        cosphi, sinphi = np.cos(np.pi*pan/180), np.sin(np.pi*pan/180)
+        x[:] = np.c_[cosphi*x[:,0] + sinphi*x[:,2],x[:,1], -sinphi*x[:,0] + cosphi*x[:,2]]
+        # then tilt
+        costheta, sintheta = np.cos(np.pi*tilt/180), np.sin(np.pi*tilt/180)
+        x[:] = np.c_[x[:,0], costheta*x[:,1] + sintheta*x[:,2], -sintheta*x[:,1] + costheta*x[:,2]]
+        # then do projection
+            
+    
+    def DoTask(self, snapdata):        
+        if self.params["center"] is None:
+            center = np.repeat(snapdata["Header"]["BoxSize"]*0.5,3)
+        else: center = self.params["center"]
+        if self.params["rmax"] is None:
+            rmax = snapdata["Header"]["BoxSize"]/10
+        else: rmax = self.params["rmax"]
+        if self.params["pan"] is None:
+            self.params["pan"] = 0
+        if self.params["tilt"] is None:
+            self.params["tilt"] = 0
+            
+        res = self.params["res"]
+        pos, mass, hsml = np.copy(snapdata["PartType0/Coordinates"]), np.copy(snapdata["PartType0/Masses"]), np.copy(snapdata["PartType0/SmoothingLength"]) # copy these because we don't want to modify them
+        pos -= center
+        print(pos[0])
+        self.CoordinateTransform(pos,mass,hsml)
+        
+        hsml = np.clip(hsml,2*rmax/res, 1e100)
+        sigma_gas = GridSurfaceDensity(mass, pos, hsml, np.zeros(3), 2*rmax, res=res).T
+        f = (np.log10(sigma_gas)-np.log10(1))/(np.log10(1e3)-np.log10(1))
+        plt.imsave("sigma_gas2_%s_%s.png"%(str(round(self.params["pan"])).zfill(4), str(round(self.params["tilt"])).zfill(4)), plt.get_cmap('inferno')(f))
+        # fig, ax = plt.subplots(figsize=(6,6))
+        # X = Y = np.linspace(-rmax, rmax, res)
+        # X, Y = np.meshgrid(X, Y)
+        # p = ax.pcolormesh(X, Y, sigma_gas, norm=matplotlib.colors.LogNorm(vmin=1,vmax=1e3),cmap='inferno')
+        # ax.set_aspect('equal')
+
+        # divider = make_axes_locatable(ax)
+        # cax = divider.append_axes("right", size="5%", pad=0.0)
+        # fig.colorbar(p,label=r"$\Sigma_{\rm gas}$ $(\rm M_\odot\,pc^{-2})$",cax=cax)
+        # ax.set_xlabel("X (pc)")
+        # ax.set_ylabel("Y (pc)")
+        # plt.savefig("sigma_gas2_%s_%s.png"%(str(round(self.params["pan"])).zfill(4), str(round(self.params["tilt"])).zfill(4)),bbox_inches='tight',dpi=300)
+        # plt.close()
+#        print("done")
+        return sigma_gas    
