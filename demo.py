2e3fe6a5a1e155bb9c207204ea3e83dd94ecdb76
Mike Grudić <mike.grudich@gmail.com>
2021-08-02 15:56:18 +0000

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,18 +1,14 @@
 from glob import glob
 from CrunchSnaps import *
 from tasks import *
-from multiprocessing import Pool, cpu_count
-from tasks import *
 from natsort import natsorted
-import h5py
-import itertools
 import numpy as np
 from sys import argv
 
-tasks = [SinkVisCoolMap,]
-snaps = glob(argv[1]+"/snapshot*.hdf5")
-#Nparams = len(snaps)
-#Tmax = 6.9e-3
+tasks = [SinkVisCoolMap,] # select the tasks that you want to run
+snaps = natsorted(glob(argv[1]+"/snapshot*.hdf5")) # get the list of snapshots to run on (should sort chronologically for best access pattern)
+
+# Now here we generate a list of parameters for each frame we want to make, this will be done "by hand" and all unintialized parameters will adopt default values
 params = []
 Nangle = 1080
 for i in list(range(Nangle)):
@@ -27,17 +23,8 @@ for i in range(Nangle-1,Nangle+360):
     params.append({"Time": 5e-3, "pan": float(i),  "rmax": 10*np.exp(np.cos((5e-3)/1e-3)**2), "filename": "cool__%s.png"%str(i).zfill(4),"res": 256})
 i0 = i
 for i in range(i0+1,i0+360):
-    params.append({"Time": 5e-3+(6.9e-3-5e-3)*(i-i0)/360, "pan": float(i), "rmax": 10*np.exp(np.cos((5e-3)/1e-3)**2) + (i-i0)/360 * 10, "filename": "cool__%s.png"%str(i).zfill(4), "res": 256})    
+    params.append({"Time": 5e-3+(6.9e-3-5e-3)*(i-i0)/360, "pan": float(i), "rmax": 10*np.exp(np.cos((5e-3)/1e-3)**2) + (i-i0)/360 * 10, "filename": "cool__%s.png"%str(i).zfill(4), "res": 256})
 
-params = params[1000:1100]
-
-params = [params,]
-Nchunks = 16 #cpu_count()
-#params_chunks = [params[i*len(params)//Nchunks:(i+1)*len(params)//Nchunks] for i in range(Nchunks)]
-
-
-#def DoStuff(params_chunk):
-DoTasksForSimulation(snaps=snaps, tasks=tasks, task_params=params,nproc=Nchunks)
-
-#[DoStuff(chunk) for chunk in params_chunks]
-#Pool(Nchunks).map(DoStuff, params_chunks)
+params = [params,] # format as a list of lists with one entry per task
+Nchunks = 16 
+DoTasksForSimulation(snaps=snaps, tasks=tasks, task_params=params,nproc=16)
