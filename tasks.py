f3a307e8020f24dced23ac9c70b9b239a7283f92
Mike Grudic <mike.grudich@gmail.com>
2021-08-03 00:37:12 +0000

    added fresco plotting to SinkVis class

--- tasks.py
+++ tasks.py
@@ -173,7 +173,7 @@ class SinkVis(Task):
         if self.params["backend"]=="PIL":            
             fname = self.params["filename"]
             if self.params["fresco_stars"]: # use fresco for stellar images
-                data_stars_fresco = SinkVis_amuse_fresco.make_amuse_fresco_stars_only(X_star,m_star,np.zeros_like(m_star),2*self.params["rmax"],res=self.params["res"],vmax=self.params["fresco_param"],mass_rescale=self.params["fresco_mass_rescale"],mass_limits=self.params["fresco_mass_limits"])
+                data_stars_fresco = amuse_fresco.make_amuse_fresco_stars_only(X_star,m_star,np.zeros_like(m_star),2*self.params["rmax"],res=self.params["res"],vmax=self.params["fresco_param"],mass_rescale=self.params["fresco_mass_rescale"],mass_limits=self.params["fresco_mass_limits"])
                 img = plt.imread(fname)
                 plt.imsave(fname,np.clip(img[:,:,:3]+data_stars_fresco,0,1))
             else: # use derpy PIL circles
