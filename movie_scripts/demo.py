b3ab9548410e04073975cbd11c45e41a4c1a59a6
Michael Grudic <mgrudic@login3.frontera.tacc.utexas.edu>
2021-08-19 15:14:43 -0500

    small tweaks to demo flythrough movie script

--- movie_scripts/demo.py
+++ movie_scripts/demo.py
@@ -11,23 +11,49 @@ snaps = natsorted(glob(argv[1]+"/snapshot*.hdf5")) # get the list of snapshots t
 # Now here we generate a list of parameters for each frame we want to make, this will be done "by hand" and all unintialized parameters will adopt default values
 Nangle = 1080
 params = []
+tilt = 0
+pan = 0
+pan_seconds_per_rotation = 30
+fps = 24
+pan_deg_per_frame = 360 / (fps * pan_seconds_per_rotation)
+res = 1920 #3840
+frame = 0
+Tmax = 7.65e-3
+center = np.float64([50,50,50])
+tot_frames = Nangle + 10*fps + 360 + pan_seconds_per_rotation * fps
+
+centers = center + np.zeros((tot_frames,3)) #np.c_[8*np.cos(3*np.pi*np.arange(tot_frames)/tot_frames), np.zeros(tot_frames),8*np.sin(6*np.pi*np.arange(tot_frames)/tot_frames)]
+
 for i in list(range(Nangle)):
+    tilt = 25* np.sin(2*np.pi*i/Nangle)
     params.append({"Time": 5e-3 * i/Nangle,
-                   "pan": float(i)/Nangle * 360,
-                   "tilt": 30*np.sin(2*np.pi*i/Nangle),                   
-                   "rmax": 10*(1+np.exp(-5*i/Nangle)),
-                   "filename": "cool__%s.png"%str(i).zfill(4),
-                   "res": 1024}
+                   "pan": pan,
+                   "tilt": tilt,                   
+                   "focal_distance": 8*(1+4*np.exp(-5*i/Nangle)),
+                   "filename": "cool__%s.png"%str(frame).zfill(4),
+                   "res": res,
+                   "fresco_stars": True,
+                   "center": centers[frame],
+                   "no_timestamp": True
+               }
     )
-for i in range(Nangle-1,Nangle+360):
-    params.append({"Time": 5e-3, "pan": float(i),  "rmax": 10, "filename": "cool__%s.png"%str(i).zfill(4),"res": 1024})
+    frame += 1
+    if i > fps * 5: pan += pan_deg_per_frame
+for i in range(10 * fps):
+    params.append({"Time": 5e-3, "pan": pan, "tilt": tilt,  "focal_distance": 8 * np.exp(0.5*(-1+np.cos(2*np.pi*i/(10*fps)))), "filename": "cool__%s.png"%str(frame).zfill(4),"res": res, "fresco_stars": True,  "center": centers[frame],"no_timestamp": True})
+    frame += 1
+    pan += pan_deg_per_frame
+
 i0 = i
 for i in range(i0+1,i0+360):
-    params.append({"Time": 5e-3+(7e-3-5e-3)*(i-i0)/360, "pan": float(i0) + (i-i0)*0.3, "rmax": 10 + (i-i0)*10/360, "filename": "cool__%s.png"%str(i).zfill(4), "res": 1024})    
-i0 = i
-for i in range(i0+1,i0+1080):
-    params.append({"Time": 7e-3, "pan": float(i0) + (i-i0)*0.3, "rmax": 10 + (i-i0)*10/1080, "filename": "cool__%s.png"%str(i).zfill(4), "res": 1024})    
+    params.append({"Time": 5e-3+(Tmax-5e-3)*(i-i0)/360, "pan": pan, "focal_distance": 8 + (i-i0)*10/360, "filename": "cool__%s.png"%str(frame).zfill(4), "res": res, "fresco_stars": True,   "center": centers[frame],"no_timestamp": True})    
+    frame += 1
 
-params = [params] # format as a list of lists with one entry per task
-Nchunks = 16 
+for i in range(pan_seconds_per_rotation * fps):
+    pan += pan_deg_per_frame
+    tilt = np.sin(3*np.pi*i/(pan_seconds_per_rotation*fps))
+    params.append({"Time": Tmax, "pan": pan, "focal_distance": 18 * np.exp(np.sin(1.5*np.pi*i/(pan_seconds_per_rotation*fps))), "filename": "cool__%s.png"%str(frame).zfill(4), "res": res, "tilt": tilt, "fresco_stars": True,   "center": centers[frame],"no_timestamp": True})    
+    frame += 1
+print(len(params))
+params = [params,] # format as a list of lists with one entry per task
 DoTasksForSimulation(snaps=snaps, tasks=tasks, task_params=params,nproc=16)
