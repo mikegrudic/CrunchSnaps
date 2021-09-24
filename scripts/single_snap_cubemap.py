a61562b5e305420ebf9374392c71eaa7bfeef631
Mike Grudic <mike.grudich@gmail.com>
2021-09-23 21:21:44 -0700

    single snap cubemap script

---
+++ scripts/single_snap_cubemap.py
@@ -0,0 +1,18 @@
+import h5py
+from CrunchSnaps import *
+from sys import argv
+
+task = SinkVisSigmaGas
+
+for f in argv[1:]:
+    snapnum = f.split("snapshot_")[1].split(".hdf5")[0]
+    params = {"camera_distance": 0, "Time": 0, "fresco_stars": True, "index": int(snapnum), "pan": 0, "no_timestamp": True, "threads": -1, "no_stars": False, "res": 1920, "cmap": "inferno", "limits": [10,1e4],"tilt": -90}
+    params = cubemapify(params)
+    snapdata = None
+    for p in params:
+        print(p)
+        t = task(p)
+        if not snapdata:
+            fields = t.GetRequiredSnapdata()        
+            snapdata = GetSnapData(f, fields)
+        t.DoTask(snapdata)
